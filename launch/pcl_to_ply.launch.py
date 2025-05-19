d4d6ad702a7aefea14ab9bd73d9b013f4bd28da7
Jan Kuhlmann <33833587+M2-TE@users.noreply.github.com>
2025-05-19 14:48:45 +0200

    added ply writer

---
+++ launch/pcl_to_ply.launch.py
@@ -0,0 +1,26 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    topic = LaunchConfiguration('topic', default='/robot/dlio/odom_node/pointcloud/deskewed')
+
+    declare_pointcloud_topic_arg = DeclareLaunchArgument(
+        'topic',
+        default_value=topic,
+        description='Pointcloud topic name'
+    )
+    node = Node(
+        package='pcl_to_ply',
+        executable='pcl_to_ply',
+        name='pcl_to_ply',
+        output='screen',
+        remappings=[
+            ('pointcloud', topic),
+        ]
+    )
+    return LaunchDescription([
+        declare_pointcloud_topic_arg,
+        node
+    ])
--- src/pcl_to_ply.cpp
+++ src/pcl_to_ply.cpp
@@ -1,4 +1,3 @@
-#include <memory>
 #include <fstream>
 #include <Eigen/Eigen>
 #include <rclcpp/rclcpp.hpp>
@@ -32,21 +31,28 @@ POINT_CLOUD_REGISTER_POINT_STRUCT(Point,
 struct pcl_to_ply: public rclcpp::Node {
     pcl_to_ply(): Node("pcl_to_ply") {
         _sub_pcl = this->create_subscription<sensor_msgs::msg::PointCloud2>(
-            "/ouster/points", 
+            "/pointcloud", 
             100,
             std::bind(&pcl_to_ply::callback_points, this, std::placeholders::_1)
         );
 
-        _ofs.open("pointcloud.ply");
+        std::ios::sync_with_stdio(false);
+        _ofs.open("pointcloud.ply", std::ios::binary);
+        _ofs.rdbuf()->pubsetbuf(_io_buffer.data(), _io_buffer.size());
         _ofs << "ply\n";
         _ofs << "format ascii 1.0\n";
-        _ofs << "element vertex \\(vertexCount)\n";
+        // _ofs << "format binary_little_endian 1.0\n";
+        _ofs << "element vertex                         \n";
         _ofs << "property float x\n";
         _ofs << "property float y\n";
         _ofs << "property float z\n";
         _ofs << "end_header\n";
     }
     ~pcl_to_ply() {
+        // write vertex count
+        _ofs.seekp(36, std::ios::beg);
+        // _ofs.seekp(51, std::ios::beg);
+        _ofs << _point_count;
         _ofs.close();
     }
 
@@ -54,12 +60,16 @@ struct pcl_to_ply: public rclcpp::Node {
         // extract pointcloud from message
         pcl::PointCloud<Point> pointcloud = {};
         pcl::fromROSMsg(msg, pointcloud);
+        std::cout << "Received " << pointcloud.points.size() << " points" << std::endl;
         for (const auto& point: pointcloud.points) {
             _ofs << point.x << ' ' << point.y << ' ' << point.z << '\n';
         }
+        _point_count += pointcloud.points.size();
     }
 
     std::ofstream _ofs;
+    std::size_t _point_count;
+    std::array<char, 256*1024> _io_buffer;
     rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr _sub_pcl;
 };
 
